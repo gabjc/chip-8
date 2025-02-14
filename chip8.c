6877d2a74e4b511f9df653d95cb553606e39b1d0
gabjc <gabtez23@gmail.com>
2025-02-14 17:17:22 -0500

    set up fontset, opcode executions, and timer

--- chip8.c
+++ chip8.c
@@ -1,4 +1,5 @@
 #include "chip8.h"
+#include "stdint.h"
 
 void initialize(void) {
     // Initialize registers and memory once
@@ -22,24 +23,41 @@ void initialize(void) {
     sound_timer = 0;
   }
 
-  
+void loadGame(char game) {
+  // Have to use fopen in binary mode to fill the memory location at 0x200 == 512
+  for(int i = 0; i < bufferSize; ++i) {
+    memory[i + 512] = buffer[i];
+  }
+
+}
   
 void emulateCycle() {
   // Fetch Opcode
-  // Decode Opcode
-  // Execute Opcode
+  fetchOpcode(memory, pc);
 
+  // Decode Opcode
+  decodeOpcode(opcode);
+  
   // Update timers
+  updateTimers();
 }
 
 
 unsigned short fetchOpcode(unsigned char *memory[4096], unsigned short *pc) {
-  unsigned short opcode = *memory[*pc] << 8 | *memory[*pc + 1];
+  opcode = *memory[*pc] << 8 | *memory[*pc + 1];
   return opcode;
 }
 
 void decodeOpcode(unsigned short *opcode) {
   switch(*opcode & 0xF000) {
+    
+    case 0xA000: // ANNN: Sets I to the address NNN
+      // Execute opcode
+      I = *opcode & 0x0FFF;
+      pc += 2;
+    break;
+    
+
     case 0x0000:
     switch(*opcode & 0x000F) {
       case 0x0000: // 0x00E0: Clears the screen        
@@ -64,5 +82,14 @@ void executeOpcode() {
 
 
 void updateTimers() {
+  if (delay_timer > 0) {
+    --delay_timer;
+  }
 
+  if (sound_timer > 0) {
+    if (sound_timer == 1) {
+      printf("BEEP!\n");
+    }
+    --sound_timer;
+  }
 }
