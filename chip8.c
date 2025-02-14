d281309d468cef5f5217872456eb79a4166c5264
gabjc <gabtez23@gmail.com>
2025-02-14 00:04:47 -0500

    started opcode handling

--- chip8.c
+++ chip8.c
@@ -15,11 +15,45 @@ void initialize(void) {
     unsigned char key[16];
   }
   
-  void emulateCycle() {
-    // Fetch Opcode
-    // Decode Opcode
-    // Execute Opcode
-  
-    // Update timers
-  }
+void emulateCycle() {
+  // Fetch Opcode
+  // Decode Opcode
+  // Execute Opcode
 
+  // Update timers
+}
+
+
+unsigned short fetchOpcode(unsigned char *memory[4096], unsigned short *pc) {
+  unsigned short opcode = *memory[*pc] << 8 | *memory[*pc + 1];
+  return opcode;
+}
+
+void decodeOpcode(unsigned short *opcode) {
+  switch(*opcode & 0xF000) {
+    case 0x0000:
+    switch(*opcode & 0x000F) {
+      case 0x0000: // 0x00E0: Clears the screen        
+        // Execute opcode
+      break;
+ 
+      case 0x000E: // 0x00EE: Returns from subroutine          
+        // Execute opcode
+      break;
+ 
+      default:
+        printf ("Unknown opcode [0x0000]: 0x%X\n", opcode);          
+    }
+  break;
+  }
+}
+
+
+void executeOpcode() {
+
+}
+
+
+void updateTimers() {
+
+}
